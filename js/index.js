549c998399dea22307f4ce72d7de7cf2207e57b7
piedasing <a0939732111@gmail.com>
2018-04-13 09:32:23 +0800

    complete

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
+//點擊continue按鈕後，顯示導覽列、產品、footer
 function start(){
-  $("#nav").css("display","flex")
+  $("#nav").css("display","block")
   $("#product").css("display","block")
   $("#footer").css("display","block")
   $("#home").css("display","none")
 }
-
+//點擊導覽列的首頁名稱，回到首頁，並把其他隱藏
 function home(){
   $("#home").css("display","block")
   $("#nav").css("display","none")
@@ -14,7 +15,7 @@ function home(){
   $("#QA").css("display","none")
   $("#footer").css("display","none")
 }
-
+//點擊導覽列項目，把其他不相關的區塊隱藏
 function fn(i){
   $("#footer").css("display","block")
   $("#home").css("display","none")
@@ -47,3 +48,11 @@ function fn(i){
 
   }
 }
+//螢幕小於480px，點擊選單項目後會自動收合(再點一次按鈕)
+$(function(){
+  if($(window).width() < 480){
+    $(".navbar a").on("click",function(){
+      $(".navbar-toggler").click();
+    })
+  }
+})
