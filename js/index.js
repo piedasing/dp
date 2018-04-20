c526cd6c02c1fb3619a95242e25407c3b232dff9
piedasing <a0939732111@gmail.com>
2018-04-20 08:52:31 +0800

    20180420

--- js/index.js
+++ js/index.js
@@ -56,3 +56,91 @@ $(function(){
     })
   }
 })
+
+cards = [
+  {
+    "id": "product",
+    "title": "即將上市",
+    "label": "pro01_label",
+    "target": "#pro01",
+    "target_id": "pro01",
+    "img": "./img/product/01.jpg"
+  },
+  {
+    "id": "experiment",
+    "title": "即將上市",
+    "label": "exp01_label",
+    "target": "#exp01",
+    "target_id": "exp01",
+    "img": "./img/product/01.jpg"
+  },
+  {
+    "id": "product",
+    "title": "即將上市",
+    "label": "pro02_label",
+    "target": "#pro02",
+    "target_id": "pro02",
+    "img": "./img/product/01.jpg"
+  },
+  {
+    "id": "product",
+    "title": "即將上市",
+    "label": "pro03_label",
+    "target": "#pro03",
+    "target_id": "pro03",
+    "img": "./img/product/01.jpg"
+  },
+  {
+    "id": "product",
+    "title": "即將上市",
+    "label": "pro04_label",
+    "target": "#pro04",
+    "target_id": "pro04",
+    "img": "./img/product/01.jpg"
+  },
+  {
+    "id": "product",
+    "title": "即將上市",
+    "label": "pro05_label",
+    "target": "#pro05",
+    "target_id": "pro05",
+    "img": "./img/product/01.jpg"
+  },
+  {
+    "id": "product",
+    "title": "即將上市",
+    "label": "pro06_label",
+    "target": "#pro06",
+    "target_id": "pro06",
+    "img": "./img/product/01.jpg"
+  },
+  {
+    "id": "product",
+    "title": "即將上市",
+    "label": "pro07_label",
+    "target": "#pro07",
+    "target_id": "pro07",
+    "img": "./img/product/01.jpg"
+  },
+  {
+    "id": "product",
+    "title": "即將上市",
+    "label": "pro08_label",
+    "target": "#pro08",
+    "target_id": "pro08",
+    "img": "./img/product/01.jpg"
+  }
+]
+
+
+Vue.component("card",{
+  template: "#card",
+  props: ["card_data"]
+})
+
+var vm = new Vue({
+  el: "#app",
+  data: {
+    cards: cards
+  }
+})
