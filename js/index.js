62d06535bd57ef3bf580d46c7424df06b4de3786
piedasing <a0939732111@gmail.com>
2018-04-21 09:24:38 +0800

    0421

--- js/index.js
+++ js/index.js
@@ -1,102 +1,358 @@
-cards = [
+card_product = [
   {
     "id": "product",
-    "title": "即將上市",
     "target": "#pro01",
     "target_id": "pro01",
-    "img": "./img/product/01.jpg",
-    "intro": "intro1\n\n\nintro1intro1intro1intro1intro1"
-  },
-  {
-    "id": "experiment",
-    "title": "即將上市",
-    "target": "#exp01",
-    "target_id": "exp01",
-    "img": "./img/product/01.jpg",
-    "intro": "intro1intro1intro1intro1intro1intro1"
-  },
-  {
-    "id": "experiment",
-    "title": "即將上市",
-    "target": "#exp01",
-    "target_id": "exp01",
-    "img": "./img/product/01.jpg",
-    "intro": "intro1intro1intro1intro1intro1intro1"
-  },
-  {
-    "id": "experiment",
-    "title": "即將上市",
-    "target": "#exp01",
-    "target_id": "exp01",
-    "img": "./img/product/01.jpg",
-    "intro": "intro1intro1intro1intro1intro1intro1"
-  },
-  {
-    "id": "experiment",
-    "title": "即將上市",
-    "target": "#exp01",
-    "target_id": "exp01",
-    "img": "./img/product/01.jpg",
-    "intro": "intro1intro1intro1intro1intro1intro1"
+    "data": {
+      "title": "即將上市",
+      "img": "./img/product/01.jpg"
+    },
+    "data_intro": {
+      "title": "敬請期待",
+      "img": "./img/product/01.jpg",
+      "intro": ""
+    }
   },
   {
     "id": "product",
-    "title": "即將上市",
     "target": "#pro02",
     "target_id": "pro02",
-    "img": "./img/product/01.jpg",
-    "intro": "intro1intro1intro1intro1intro1intro1"
+    "data": {
+      "title": "即將上市",
+      "img": "./img/product/01.jpg"
+    },
+    "data_intro": {
+      "title": "敬請期待",
+      "img":"./img/product/01.jpg",
+      "intro": ""
+    }
   },
   {
     "id": "product",
-    "title": "即將上市",
     "target": "#pro03",
     "target_id": "pro03",
-    "img": "./img/product/01.jpg",
-    "intro": "intro1intro1intro1intro1intro1intro1"
+    "data": {
+      "title": "即將上市",
+      "img": "./img/product/01.jpg"
+    },
+    "data_intro": {
+      "title": "敬請期待",
+      "img":"./img/product/01.jpg",
+      "intro": ""
+    }
   },
   {
     "id": "product",
-    "title": "即將上市",
     "target": "#pro04",
     "target_id": "pro04",
-    "img": "./img/product/01.jpg",
-    "intro": "intro1intro1intro1intro1intro1intro1"
+    "data": {
+      "title": "即將上市",
+      "img": "./img/product/01.jpg"
+    },
+    "data_intro": {
+      "title": "敬請期待",
+      "img":"./img/product/01.jpg",
+      "intro": ""
+    }
   },
   {
     "id": "product",
-    "title": "即將上市",
     "target": "#pro05",
     "target_id": "pro05",
-    "img": "./img/product/01.jpg",
-    "intro": "intro1intro1intro1intro1intro1intro1"
+    "data": {
+      "title": "即將上市",
+      "img": "./img/product/01.jpg"
+    },
+    "data_intro": {
+      "intro_title": "敬請期待",
+      "intro_img":"./img/product/01.jpg",
+      "intro": ""
+    }
   },
   {
     "id": "product",
-    "title": "即將上市",
     "target": "#pro06",
     "target_id": "pro06",
-    "img": "./img/product/01.jpg",
-    "intro": "intro1intro1intro1intro1intro1intro1"
+    "data": {
+      "title": "即將上市",      
+      "img": "./img/product/01.jpg"
+    },
+    "data_intro": {
+      "intro_title": "敬請期待",
+      "intro_img":"./img/product/01.jpg",
+      "intro": ""
+    }
   },
   {
     "id": "product",
-    "title": "即將上市",
     "target": "#pro07",
     "target_id": "pro07",
-    "img": "./img/product/01.jpg",
-    "intro": "intro1intro1intro1intro1intro1intro1"
+    "data": {
+      "title": "即將上市",
+      "img": "./img/product/01.jpg"
+    },
+    "data_intro": {
+      "intro_title": "敬請期待",
+      "intro_img":"./img/product/01.jpg",
+      "intro": ""
+    }
   },
   {
     "id": "product",
-    "title": "即將上市",
     "target": "#pro08",
     "target_id": "pro08",
-    "img": "./img/product/01.jpg",
-    "intro": "intro1intro1intro1intro1intro1intro1"
+    "data": {
+      "title": "即將上市",
+      "img": "./img/product/01.jpg"
+    },
+    "data_intro": {
+      "intro_title": "敬請期待",
+      "intro_img":"./img/product/01.jpg",
+      "intro": ""
+    }
   }
 ]
 
+card_experiment = [
+  {
+    "id": "experiment",
+    "target": "#exp01",
+    "target_id": "exp01",
+    "data": {
+      "title": "冷啟動模擬器",
+      "img": "./img/experiment/01-s.jpg"
+    },
+    "data_intro": {
+      "title": "CCS : 冷啟動模擬器 \nASTM : D5293-10",
+      "img":"./img/experiment/01.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp02",
+    "target_id": "exp02",
+    "data": {
+      "title": "高溫高剪切測定儀",
+      "img": "./img/experiment/02-s.jpg"
+    },
+    "data_intro": {
+      "title": "HTHS : 高溫高剪切測定儀\nASTM : ASTM D-5481-10",
+      "img":"./img/experiment/02.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp03",
+    "target_id": "exp03",
+    "data": {
+      "title": "消泡測試儀",
+      "img": "./img/experiment/03-s.jpg"
+    },
+    "data_intro": {
+      "title": "ASTM:D892",
+      "img":"./img/experiment/03.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp04",
+    "target_id": "exp04",
+    "data": {
+      "title": "PPD低溫流動測定儀",
+      "img": "./img/experiment/04-s.jpg"
+    },
+    "data_intro": {
+      "title": "ASTM : D-97",
+      "img":"./img/experiment/04.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp05",
+    "target_id": "exp05",
+    "data": {
+      "title": "MINI-ROTARY VISCOMETER",    
+      "img": "./img/experiment/05-s.jpg"
+    },
+    "data_intro": {
+      "title": "ASTM : D4684-08",
+      "img":"./img/experiment/05.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp06",
+    "target_id": "exp06",
+    "data": {
+      "title": "自動黏度測定儀",    
+      "img": "./img/experiment/06-s.jpg"
+    },
+    "data_intro": {
+      "title": "ASTM : D-445 VI\nASTM : D-2270",
+      "img":"./img/experiment/06.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp07",
+    "target_id": "exp07",
+    "data": {
+      "title": "閉杯式閃火點測定儀",    
+      "img": "./img/experiment/07-s.jpg"
+    },
+    "data_intro": {
+      "title": "ASTM : D-93",
+      "img":"./img/experiment/07.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp08",
+    "target_id": "exp08",
+    "data": {
+      "title": "開杯式閃火點測定儀",    
+      "img": "./img/experiment/08-s.jpg"
+    },
+    "data_intro": {
+      "title": "ASTM : D-92",
+      "img":"./img/experiment/08.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp09",
+    "target_id": "exp09",
+    "data": {
+      "title": "TAN、TBN 酸價、鹼價測",
+      "img": "./img/experiment/09-s.jpg"
+    },
+    "data_intro": {
+      "title": "TBN ASTM : D-2896\nTAN ASTM : D-664",
+      "img":"./img/experiment/09.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp10",
+    "target_id": "exp10",
+    "data": {
+      "title": "銅板腐蝕測試儀",
+      "img": "./img/experiment/10-s.jpg"
+    },
+    "data_intro": {
+      "title": "ASTM : D-130",
+      "img":"./img/experiment/10.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp11",
+    "target_id": "exp11",
+    "data": {
+      "title": "色相分析儀",
+      "img": "./img/experiment/11-s.jpg"
+    },
+    "data_intro": {
+      "title": "ASTM : D-1500",
+      "img":"./img/experiment/11.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp12",
+    "target_id": "exp12",
+    "data": {
+      "title": "831KF Coulometer 庫倫式水分測定",
+      "img": "./img/experiment/12-s.jpg"
+    },
+    "data_intro": {
+      "title": "ASTM : D-492",
+      "img":"./img/experiment/12.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp13",
+    "target_id": "exp13",
+    "data": {
+      "title": "乳化測定儀",
+      "img": "./img/experiment/13-s.jpg"
+    },
+    "data_intro": {
+      "title": "ASTM : D-140",
+      "img":"./img/experiment/13.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp14",
+    "target_id": "exp14",
+    "data": {
+      "title": "傅立葉紅外線光譜儀",
+      "img": "./img/experiment/14-s.jpg"
+    },
+    "data_intro": {
+      "title": "ASTM : E-1421",
+      "img":"./img/experiment/14.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp15",
+      "target_id": "exp15",
+    "data": {
+      "title": "潤滑脂錐入度測定儀",
+      "img": "./img/experiment/15-s.jpg"
+    },
+    "data_intro": {
+      "title": "ASTM : E-1421",
+      "img":"./img/experiment/15.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp16",
+    "target_id": "exp16",
+    "data": {
+      "title": "寬潤滑脂溫度範圍滴定測定儀",     
+      "img": "./img/experiment/16-s.jpg"
+    },
+    "data_intro": {
+      "title": "ASTM : D217",
+      "img":"./img/experiment/16.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp17",
+    "target_id": "exp17",
+    "data": {
+      "title": "XRF光譜",
+      "img": "./img/experiment/17-s.jpg"
+    },
+    "data_intro": {
+      "title": "XRF光譜儀\nASTM : D7751",
+      "img":"./img/experiment/17.jpg",
+      "intro": ""
+    }
+  }
+]
 
 Vue.component("card",{
   template: "#card",
@@ -106,7 +362,10 @@ Vue.component("card",{
 var vm = new Vue({
   el: "#app",
   data: {
-    cards: cards
+    cards: {
+      "card_product": card_product,
+      "card_experiment": card_experiment
+    }
   }
 })
 
@@ -138,27 +397,37 @@ function fn(i){
       $("#product").css("display","block")
       $("#experiment").css("display","none")
       $("#about").css("display","none")
+      $("#act").css("display","none")
       $("#QA").css("display","none")
       break
     case 1:
       $("#product").css("display","none")
       $("#experiment").css("display","block")
       $("#about").css("display","none")
+      $("#act").css("display","none")
       $("#QA").css("display","none")
       break
     case 2:
       $("#product").css("display","none")
       $("#experiment").css("display","none")
       $("#about").css("display","block")
+      $("#act").css("display","none")
       $("#QA").css("display","none")
       break
     case 3:
       $("#product").css("display","none")
       $("#experiment").css("display","none")
       $("#about").css("display","none")
+      $("#act").css("display","block")
+      $("#QA").css("display","none")
+      break
+    case 4:
+      $("#product").css("display","none")
+      $("#experiment").css("display","none")
+      $("#about").css("display","none")
+      $("#act").css("display","none")
       $("#QA").css("display","block")
       break
-
   }
 }
 //螢幕小於480px，點擊選單項目後會自動收合(再點一次按鈕)
